99c324f838668f4200ebbcd1f9b6b9c0030ce2a0
Djangirkhan Tuleov <89830596+jeanguir@users.noreply.github.com>
2024-08-04 20:09:29 +0500

    Create TaskList.java

---
+++ TaskList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(String description) {
+        tasks.add(new Task(description));
+    }
+
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+        } else {
+            System.out.println("Invalid task index.");
+        }
+    }
+
+    public void markTaskCompleted(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markCompleted();
+        } else {
+            System.out.println("Invalid task index.");
+        }
+    }
+
+    public void listTasks() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + ": " + tasks.get(i));
+        }
+    }
+}
